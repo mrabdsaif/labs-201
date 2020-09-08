ea817da259790a686f6b2c1157e90f8034ee9cb4
mrabdsaif <abdullah.saif11@yahoo.com>
2020-09-08 13:51:53 +0300

    new-updated

---
+++ lab03-updated-aboutMe/js/script.js
@@ -0,0 +1,53 @@
+'use strict';
+var userName = prompt('What Is your Name?'); userName.toLowerCase();
+alert('Welcome Mr/Ms. ' + userName + ' To Abdullah Blog');
+var whoWantToGus =confirm('Would You Like to Play a Gusseng Game Mr/Ms. ' + userName);
+//If He/she Does not want to play and choose no how can we end the script right here!!
+alert(' Mr/Ms ' + userName + ' Your answers Shoud be y/n or yes/no');
+var majorName = '';
+    majorName =prompt('IS my Major Statistc?'); majorName.toLowerCase();
+if (majorName == 'n' || majorName === 'no'){
+    alert('Wrong Answer, My major Is statistic');
+    console.log('Wrong Answer, My Major Is statistic');
+}else{
+    alert('Correct Answer');
+console.log('Correct Answer My Major is Statistic');
+}
+var uniName='';
+    uniName = prompt('My University Name was University of Jordan?'); uniName.toLowerCase();
+if (uniName === 'yes' || uniName === 'y'){
+    alert('Correct Anser');
+console.log('You Are Gamer Man, Perfect');
+}else{
+    alert('Your Answer Is Wrong, My Uni Name was Uni Of Jordan');
+    console.log('Your Answer Is Wrong, My Uni Name was Uni Of Jordan');
+}
+var graduatedYear= 2010; //How Can I define numarical Variables without assigning values!!
+    graduatedYear= prompt(' My Grad Year was in 2010?'); graduatedYear.toLowerCase();
+if(graduatedYear !== 'no' && graduatedYear !== 'n'){
+    alert('You are A big Gamer My dear, Correct Answer');
+    console.log('Correct Answer');
+}else{
+    alert('Your Answer Is Wrong, My Grad Year Was in 2010');
+}
+var firstWork= '';
+    firstWork= prompt('My first Work was Data analyst?'); firstWork.toLowerCase();
+if(firstWork !== 'no' && firstWork !== 'n'){
+    alert('You are A big Gamer My dear, Correct Answer');
+    console.log('Correct Answer');
+}else{
+    alert('Your Answer Is Wrong, My first work was Data analyst');
+    console.log('Your Answer Is Wrong, My first work was Data analyst');
+}
+
+   whoWantToGus=confirm('Are You Getting Enough Joy Dear? Would you like to go on?');
+
+var secondWork= 'Methodologist';
+    secondWork= prompt('My Second Work was Methodologist?'); secondWork.toLowerCase();
+if(secondWork !== 'no' && secondWork !== 'n'){
+    alert('You are A big Gamer My dear, Correct Answer');
+    console.log('Correct Answer');
+}else{
+    alert('Your Answer Is Wrong, My Second work was Metho..');
+    console.log('Your Answer Is Wrong, My Second work was Metho..');
+}
